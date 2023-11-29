b097278bef18e5fe0865ae12aa61b13a6f5a16e1
Ranjit Das <ranjitda@creative.local>
2023-11-29 17:45:16 +0530

    Up[dated the while loop from laptop

--- app/src/main/java/com/example/rockpaperscissor/RockPaperScissors.kt
+++ app/src/main/java/com/example/rockpaperscissor/RockPaperScissors.kt
@@ -7,6 +7,10 @@ fun main(){
     var playerChoice = ""
     println("Rock, Paper or Scissor? Enter your choice")
     playerChoice = readln()
+    while ((playerChoice != "Scissor") && (playerChoice != "Paper") && (playerChoice != "Rock")) {
+        println("Please give a valid input")
+        playerChoice = readln()
+    }
 
     val randomNumber = (1..3).random()
 
@@ -27,15 +31,11 @@ fun main(){
         playerChoice == "Rock" && computerChoice == "Scissor" -> "Player"
         playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
         playerChoice == "Scissor" && computerChoice == "Paper" -> "Player"
-        playerChoice !== "Scissor" && playerChoice !== "Paper" && playerChoice !== "Rock" -> "Error"
         else -> "Computer"
     }
     when(winner){
         "Draw" -> {
             println("Computer's choice is also same so the match is draw")
-        }
-        "Error" -> {
-            println("Please enter a valid input")
         }
         else -> {
             println("Player's choice is $playerChoice and Computer's choice is $computerChoice so the winner is $winner")
